ad9896380b40c5082fe0f04d9fa072cc8b486b2b
Hossein Zahed <hzahed@outlook.com>
2025-04-16 12:57:07 +0200

    chainlit oauth configured

--- src/Playground.ChainlitUI/app.py
+++ src/Playground.ChainlitUI/app.py
@@ -1,5 +1,25 @@
+from typing import Dict, Optional
 import chainlit as cl
 
+
+# OAuth callback for authentication
+# This function is called when the user successfully authenticates with the OAuth provider.
+@cl.oauth_callback
+async def oauth_callback(
+    provider_id: str,
+    token: str,
+    raw_user_data: Dict[str, str],
+    default_user: cl.User,
+) -> Optional[cl.User]:
+    print(f"OAuth callback for provider {provider_id}")
+    default_user.identifier = raw_user_data["mail"]
+    default_user.display_name = raw_user_data["displayName"]
+    default_user.metadata["user_id"] = raw_user_data["id"]
+    default_user.metadata["first_name"] = raw_user_data["givenName"]
+    default_user.metadata["job_title"] = raw_user_data["jobTitle"]
+    default_user.metadata["office_location"] = raw_user_data["officeLocation"]
+    return default_user
+    
 
 @cl.on_message
 async def main(message: cl.Message):
